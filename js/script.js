527ae1dcd6cd2287fafa1ca17bbce77904b364b2
Wanderson <wanderson.wf@pg.com>
2022-06-02 09:00:53 -0400

    Finalisado! v-0.1

---
+++ js/script.js
@@ -0,0 +1,18 @@
+document.getElementById('input_url').addEventListener('keyup', () => {
+    let btnGerar = document.getElementById('gerar');
+    btnGerar.className = 'btn btn-primary'
+});
+
+document.getElementById('gerar').addEventListener('click', () => {
+    let url = document.getElementById('input_url').value;
+
+    linkURL = document.getElementById('linkURL')
+    linkURL.innerText = 'Seu link';
+    linkURL.className = 'btn btn-success';
+    linkURL.target = '_blank'
+    linkURL.href = url;
+
+
+    document.getElementById('qrcode').innerHTML = 
+    '<img class="rounded" src="https://api.qrserver.com/v1/create-qr-code/?data=' + url + '&size=300x300" alt="" title="" />'
+})
